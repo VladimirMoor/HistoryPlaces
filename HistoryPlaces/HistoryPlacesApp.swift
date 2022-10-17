d87bc078030779e882f84f837a012e3dcb24fa68
Vladimir Muravev <vladimircode@icloud.com>
2022-10-17 09:28:22 +0300

    Initial Commit

---
+++ HistoryPlaces/HistoryPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryPlacesApp.swift
+//  HistoryPlaces
+//
+//  Created by Владимир Муравьев on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct HistoryPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
