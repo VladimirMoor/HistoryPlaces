e318371ffdc94b50bdddb0d742b0a1422bdf2aa6
Vladimir Muravev <vladimircode@icloud.com>
2022-10-17 12:51:37 +0300

    setup map location

--- HistoryPlaces/ViewModels/LocationsViewModel.swift
+++ HistoryPlaces/ViewModels/LocationsViewModel.swift
@@ -5,12 +5,34 @@
 //  Created by Владимир Муравьев on 17.10.2022.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
 
 class LocationsViewModel: ObservableObject {
+    
+    // All loaded locations
     @Published var locations: [Location]
     
+    // Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
     init() {
-        self.locations = LocationsDataService.locations
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
     }
 }
