8b5c9e8d969bfbfa8a3eef76d9dd6c441d1bd688
Vladimir Muravev <vladimircode@icloud.com>
2022-10-17 10:13:00 +0300

    Project Setup, crating Model

--- HistoryPlaces/ContentView.swift
+++ HistoryPlaces/ContentView.swift
@@ -11,8 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
                 .foregroundColor(.accentColor)
+                .font(.system(size: 100))
             Text("Hello, world!")
         }
         .padding()
---
+++ HistoryPlaces/Models/Location.swift
@@ -0,0 +1,21 @@
+//
+//  Location.swift
+//  HistoryPlaces
+//
+//  Created by Владимир Муравьев on 17.10.2022.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    
+}
